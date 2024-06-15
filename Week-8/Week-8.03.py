69f8fc9e4908396eb4531949c8b3a3e6399b618d
Jegaeswaran-A <231001075@rajalakshmi.edu.in>
2024-06-15 13:55:55 +0530

    Week-8.03.py

--- Week-8/Week-8.03.py
+++ Week-8/Week-8.03.py
@@ -1,7 +1,7 @@
 DNA Sequence
 
 The DNA sequence is composed of a series of nucleotides abbreviated as 'A', 'C', 'G', and 'T'.
-
+ 
 For example, "ACGAATTCCG" is a DNA sequence.
 
 When studying DNA, it is useful to identify repeated sequences within the DNA.
