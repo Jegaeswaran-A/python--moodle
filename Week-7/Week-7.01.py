0d0f16c0e5305366ad2d86714f90d5920611934a
Jegaeswaran-A <231001075@rajalakshmi.edu.in>
2024-06-15 13:51:48 +0530

    Week-7.01.py

--- Week-7/Week-7.01.py
+++ Week-7/Week-7.01.py
@@ -7,7 +7,7 @@ An abundant number is a number for which the sum of its proper divisors is great
 Input Format:
 
 Take input an integer from stdin
-
+ 
 Output Format:
 
 Return Yes if given number is Abundant. Otherwise, print No
