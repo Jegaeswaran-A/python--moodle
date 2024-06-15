75fd2889fb31d859bfc5f74abfd84a567da5f49d
Jegaeswaran-A <231001075@rajalakshmi.edu.in>
2024-06-15 13:56:20 +0530

    Week-8.04.py

--- Week-8/Week-8.04.py
+++ Week-8/Week-8.04.py
@@ -2,7 +2,7 @@ Print repeated no
 
 Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.There is only one repeated number in nums, return this repeated number. Solve the problem using set.
 
-Example 1:
+Example 1: 
 
 Input: nums = [1,3,4,2,2]
 
